fde968606850bd70d8f730a063f809fe6364616d
sesegu <149153075+sesegu@users.noreply.github.com>
2023-12-29 17:34:15 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://sesegu.x10.mx/stv/");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://www.baidu.com/");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
